977e91bcb82a20c05651134bc0467c05f0d447aa
stephan281094 <stephan281094@gmail.com>
2017-03-03 18:39:24 +0100

    Add mobile meta tags

--- example/main.js
+++ example/main.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  // Give each item a bright random color
   [].slice.call(document.querySelectorAll('.c-item')).forEach((item) => {
     const hue = Math.floor(Math.random() * 360)
     item.style.backgroundColor = `hsla(${hue}, 100%, 70%, 1)`
