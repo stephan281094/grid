63c85b702ac94193f8f904304dcbaa5bf7f323b3
stephan281094 <stephan281094@gmail.com>
2017-03-28 18:11:18 +0200

    Make build tool more modular

--- build.js
+++ build.js
@@ -8,55 +8,73 @@ const sass = require('node-sass')
 const chokidar = require('chokidar')
 const postcss = require('postcss')
 
-// Input and output files
+// Input and output files.
 const INPUT = path.join(__dirname, 'scss/grid.scss')
 const OUTPUT = path.join(__dirname, 'example/grid.css')
 
-// PostCSS plugins
+// PostCSS plugins.
 let plugins = [
-  require('css-mqpacker'), // Merges common media queries into one
-  require('autoprefixer') // Adds vendor prefixes
+  require('css-mqpacker'), // Merges common media queries into one.
+  require('autoprefixer') // Adds vendor prefixes.
 ]
 
 // Add PostCSS production plugins
 if (process.env.NODE_ENV === 'production') {
   plugins = [
     ...plugins,
-    require('cssnano') // Optimises and minifies CSS
+    require('cssnano') // Optimises and minifies CSS.
   ]
 }
 
-// Compile SCSS to CSS, then pipe that to PostCSS
-const build = (file) => {
+// Compile SCSS to CSS, then pipe that to PostCSS.
+const compile = (file) => {
   console.log('Compiling..')
 
-  sass.render({ file }, (err, result) => {
-    if (err || !result.css) {
-      throw new Error(
-        `Could not compile SCSS into CSS${err ? ': ' + err.message : ''}`
-      )
-    }
+  return new Promise((resolve, reject) => {
+    sass.render({ file }, (err, result) => {
+      if (err || !result.css) {
+        reject(new Error(
+          `Could not compile SCSS into CSS${err ? ': ' + err.message : ''}`
+        ))
+      }
 
-    postcss(plugins)
-      .process(result.css, {
-        from: INPUT,
-        to: OUTPUT
-      })
-      .then((result) => {
-        fs.writeFile(OUTPUT, result.css, (err) => {
-          if (err) throw err
-
-          console.log('Done!')
+      postcss(plugins)
+        .process(result.css, {
+          from: INPUT,
+          to: OUTPUT
         })
-      })
+        .then((result) => {
+          return resolve(result.css)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
   })
 }
 
-// Watch for changes if --watch has been passed. Otherwise, just build
+// Write a file with contents.
+const write = (path, contents) => {
+  fs.writeFile(path, contents, (err) => {
+    if (err) throw err
+
+    console.log(`Wrote ${path}`)
+  })
+}
+
+// Watch for changes if --watch has been passed.
 if (argv.watch) {
   console.log('Watching for changes..')
 
-  chokidar.watch(INPUT).on('change', build)
+  chokidar.watch(INPUT)
+    .on('change', (file) => {
+      compile(file).then((css) => {
+        // TODO: Emit results to allow hot reloading CSS.
+        write(OUTPUT, css)
+      })
+    })
 } else {
-  build(INPUT)
+  compile(INPUT).then((css) => {
+    write(OUTPUT, css)
+  })
 }
